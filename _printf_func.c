0a2cfe7c1351269745f39082459d9506be8c4924
B-Amal <amalboughzala1@gmail.com>
2023-07-22 11:07:52 -0700

    Task 0 - cs%

---
+++ _printf_func.c
@@ -0,0 +1,81 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdarg.h>
+
+/**
+ * convert_specifier - substitutes specifier to the corresponding arg
+ * @s: specifier
+ * @args: va_list arguments
+ *
+ * Return: number of characters printed
+ */
+int convert_specifier(char s, va_list args)
+{
+	int char_count = 0;
+
+	switch (s)
+	{
+		case 'c':
+			char_count += _putchar(va_arg(args, int));
+			break;
+		case 's':
+			char_count += _puts(va_arg(args, char *));
+			break;
+		case '%':
+			char_count += _putchar('%');
+			break;
+	}
+	return (char_count);
+}
+
+/**
+ * check_specifier - checks whether the specifier is valid
+ * @s: specifier
+ *
+ * Return: 1 when valid, 0 otherwise
+ */
+int check_specifier(char s)
+{
+	return (s == 'c' || s == 's' || s == '%');
+}
+
+/**
+ * _printf - produces output according to a format
+ * @format: format string
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int i, len, char_count = 0;
+	va_list args;
+
+	len = 0;
+	while (format[len] != '\0')
+		len++;
+
+	if (format == NULL || (len == 2 && format[0] == '%'))
+		return (-1);
+
+	va_start(args, format);
+
+	i = 0;
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%' && format[i + 1] != '\0')
+		{
+			if (check_specifier(format[i + 1]))
+			{
+				char_count += convert_specifier(format[i + 1], args);
+				i += 2;
+			}
+		}
+		else
+		{
+			char_count += _putchar(format[i]);
+			i += 1;
+		}
+	}
+	va_end(args);
+	return (char_count);
+}
