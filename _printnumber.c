b3a634664ab518914f5ac7ab5e80cb11cb58279a
B-Amal <amalboughzala1@gmail.com>
2023-07-23 07:06:42 -0700

    Update Task 1 - di

--- _printnumber.c
+++ _printnumber.c
@@ -8,34 +8,41 @@
  */
 int _printnumber(int n)
 {
-	int digit, i, j, char_count;
-	int a[11];
+	int digit, i, j, char_count = 0, is_negative = 0;
+	char a[15];
 
-	char_count = 0;
 	if (n == 0)
 	{
 		_putchar('0');
 		char_count++;
 		return (char_count);
 	}
-
+	
+	
 	if (n < 0)
 	{
-		_putchar('-');
-		char_count++;
 		n = -n;
+		is_negative++;
 	}
+
 	i = 0;
 	while (n != 0)
 	{
 		digit = n % 10;
-		a[i] = digit + '0';
+		a[char_count] = digit + '0';
 		n /= 10;
 		i++;
 		char_count++;
 	}
 
-	for (j = i - 1; a[j] != '\0'; j--)
+	if (is_negative == 1)
+	{
+		_putchar('-');
+		 char_count++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
 		_putchar(a[j]);
+
 	return (char_count);
 }
--- main.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-#include <stdio.h>
-
-int main() {
-    printf("%d\n", 65);
-    _printf("%d\n", 65);
-    return (0);
-}
